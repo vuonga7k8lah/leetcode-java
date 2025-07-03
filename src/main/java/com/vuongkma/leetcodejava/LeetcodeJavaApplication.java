d76f69f2e3e4bda41d69dc9235dbe2fdc6ca11a2
vuonga7k8lah <vuonga7k8lnc@gmail.com>
2025-07-03 15:02:38 +0700

    init code

---
+++ src/main/java/com/vuongkma/leetcodejava/LeetcodeJavaApplication.java
@@ -0,0 +1,13 @@
+package com.vuongkma.leetcodejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetcodeJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vuongkma/leetcodejava/LeetcodeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vuongkma.leetcodejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
